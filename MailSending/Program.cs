377cb10f7093c752c0b24b55f757d073137281e1
Aliya <Aliya@DESKTOP-KAFCQI4>
2021-07-06 14:00:06 +0400

    Add project files.

---
+++ MailSending/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration.Install;
+using System.Linq;
+using System.Reflection;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSending
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+
+
+            // For Debug
+
+            //var service = new MPCR_Service();
+            //service.onDebug();
+            //System.Threading.Thread.Sleep(System.Threading.Timeout.Infinite);
+
+
+
+            // FOR INSTALL
+
+            try
+            {
+                if (Environment.UserInteractive)
+                {
+                    string parameter = string.Concat("--uninstall");
+                    switch (parameter)
+                    {
+                        case "--install":
+                            ManagedInstallerClass.InstallHelper(new[] { Assembly.GetExecutingAssembly().Location });
+                            break;
+                        case "--uninstall":
+                            ManagedInstallerClass.InstallHelper(new[] { "/u", Assembly.GetExecutingAssembly().Location });
+                            break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+
+
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new MailSendingService()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
